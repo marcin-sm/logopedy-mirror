5bd76863e48f89a630af4be0e4f2b9fbd6ad353a
Marcin Smietanka <marcinsmietanka@gmail.com>
2018-05-08 22:48:18 +0200

    prototype for enabling 2 ir sensors for 2 relays

--- esp_logic/esp_logic.ino
+++ esp_logic/esp_logic.ino
@@ -1,27 +1,26 @@
-//Przykladowy kod wykorzystujacy statyczny adres IP prostego serwera opartego na ukladzie
-//ESP8266 w module WEMOS D1
-//Sterowanie mozliwe w zakresie sieci intranet
-
-
-//zalaczenie bilbioteki mikrokontrolera
-#include <ESP8266WiFi.h>
+#include <ESP8266WiFi.h>        //zalaczenie biblioteki obslugujacej WiFi w module ESP8266
 
 //deklaracja danych koniecznych do uzyskania polaczenia z routerem
 const char* ssid = "UPC62DAB45"; 
 const char* password = "Q85kaeduQdfb"; 
 
-//okreslenie pinu sprzezonego modulem przekaznika
+//okreslenie pinow sprzezonych z modulami przekaznikow
 
-int relayPin = D2;
+int relayPin = D7;
+int relayPin2 = D4;
+
+int sensorPin = D5;
+int sensorPin2 = D6;
+
 WiFiServer server(80);
 
-
-IPAddress ip(192, 168, 0, 31); // ustawienie statycznego adresu IP
-IPAddress gateway(10, 0, 0, 1); // ustawienie pasujacej bramki
-IPAddress subnet(255, 255, 255, 0); // ustawienie maski podsieci
+//IPAddress ip(192, 168, 0, 31); // ustawienie statycznego adresu IP
+//IPAddress gateway(10, 0, 0, 1); // ustawienie pasujacej bramki
+//IPAddress subnet(255, 255, 255, 0); // ustawienie maski podsieci
 
 //definicja stanu w zmiennej globalnej
-int state = LOW;
+boolean state = LOW;
+boolean state2 = LOW;
 
 // pierwsza charakterystyczna dla mikrokontrolera funkcja incjalizujaca zmienne, predkosci transmisji,
 //przeznaczenie wyprowadzen; wykonwana jednokrotnie
@@ -29,19 +28,26 @@ void setup() {
   Serial.begin(115200);
   delay(10);
  
-
   pinMode(relayPin, OUTPUT);
-  digitalWrite(relayPin, HIGH);
+  pinMode(relayPin2, OUTPUT);
+  
+  pinMode(sensorPin,INPUT_PULLUP); 
+  pinMode(sensorPin2,INPUT_PULLUP);
+  
+  digitalWrite(relayPin, HIGH);   
+  digitalWrite(relayPin2, HIGH);
  
+  //
+  
   Serial.print(F("Setting static ip to : "));
-  Serial.println(ip);
+  //Serial.println(ip);
   
   //Polaczenie z lokalna siecia WIFI
   Serial.println();
   Serial.println();
   Serial.print("Connecting to ");
   Serial.println(ssid);
-  WiFi.config(ip, gateway, subnet); 
+  //WiFi.config(ip, gateway, subnet); 
   WiFi.begin(ssid, password);
   
   //Proba polaczenia sygnalizowana przez pojawiajace sie w terminalu kropki
@@ -67,18 +73,43 @@ void setup() {
 void loop() {
   // sprawdzanie polaczenia
   WiFiClient client = server.available();
-  if (!client) {
+  if (!client && digitalRead(sensorPin)==HIGH && digitalRead(sensorPin2)==HIGH) {
     return;
   }
  
   // oczekiwanie na informacje o polaczniu
   Serial.println("new client");
-  while(!client.available()){
+  while(!client.available() && digitalRead(sensorPin)==HIGH && digitalRead(sensorPin2)==HIGH){
     delay(1);
   }
- 
+  String request;
+  if(digitalRead(sensorPin)==LOW)
+  {
+    if(state)
+    {
+      request = "GET /relay=OFF HTTP/1.1 200 OK";
+    }
+    else
+    {
+      request = "GET /relay=ON HTTP/1.1 200 OK";
+    }
+  }
+  else if(digitalRead(sensorPin2)==LOW)
+  {
+    if(state2)
+    {
+      request = "GET /relay2=OFF HTTP/1.1 200 OK";
+    }
+    else
+    {
+      request = "GET /relay2=ON HTTP/1.1 200 OK";
+    }
+  }
+  else
+  {
   // odczytanie pierwszej linii zadania
-  String request = client.readStringUntil('\r');
+  request = client.readStringUntil('\r');
+  }
   Serial.println(request);
   client.flush();
  
@@ -87,11 +118,25 @@ void loop() {
   if (request.indexOf("/relay=ON") != -1) {
     digitalWrite(relayPin, LOW);
     state = HIGH;
+    delay(200);
   } 
   if (request.indexOf("/relay=OFF") != -1){
     digitalWrite(relayPin, HIGH);
     state = LOW;
+    delay(200);
   }
+
+  if (request.indexOf("/relay2=ON") != -1) {
+    digitalWrite(relayPin2, LOW);
+    state2 = HIGH;
+    delay(200);
+  } 
+  if (request.indexOf("/relay2=OFF") != -1){
+    digitalWrite(relayPin2, HIGH);
+    state2 = LOW;
+    delay(200);
+  }
+  
   // przedstawienie odpowiedzi na stronie HTML
   
   client.println("HTTP/1.1 200 OK");
@@ -107,6 +152,13 @@ void loop() {
   } else {
     client.print("<a href=\"/relay=ON\"><img  src='https://preview.ibb.co/gEVdVH/button.png'  width = '70%'  > </a>");
   }
+
+  if(state2 == HIGH) {
+    client.print("<a href=\"/relay2=OFF\"><img  style='background:url(http://moziru.com/images/makeup-clipart-border-4.png); background-size: 100%'  src='https://preview.ibb.co/gEVdVH/button.png'   width = '70%'  > </a>");  
+  } else {
+    client.print("<a href=\"/relay2=ON\"><img  src='https://preview.ibb.co/gEVdVH/button.png'  width = '70%'  > </a>");
+  }
+
   client.println(" </center></body></html>");
  
   delay(1);
