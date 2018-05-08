a70c77ce5fc0e6e2fb898bf0257c49f4adb3940e
Marcin Smietanka <marcinsmietanka@gmail.com>
2018-05-08 22:55:10 +0200

    path issue fix

--- update.sh
+++ update.sh
@@ -1,4 +1,4 @@
 git fetch
 git reset --hard origin/master
 
-./arduino --board esp8266:esp8266:d1_mini:CpuFrequency=80,FlashSize=4M1M --port /dev/ttyUSB0 --upload /home/pi/black-mirror/esp_logic/esp_logic.ino
+/home/pi/arduino-1.8.5/arduino --board esp8266:esp8266:d1_mini:CpuFrequency=80,FlashSize=4M1M --port /dev/ttyUSB0 --upload /home/pi/black-mirror/esp_logic/esp_logic.ino
