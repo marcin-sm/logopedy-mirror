ba1e460b3bcfd886584fd86c4edd55205fabd25d
marcinsm <marcinsmietanka@gmail.com>
2018-12-17 21:50:46 +0100

    fixed casting to float

--- basic_scripts/whiteLED.py
+++ basic_scripts/whiteLED.py
@@ -2,7 +2,7 @@ import RPi.GPIO as GPIO
 import sys
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(18, GPIO.OUT)
-args = sys.argv[1:]
+args = [float(x) for x in sys.argv[1:]]
 pwm = GPIO.PWM(18, 1000)
 pwm.start(0)
 pwm.ChangeDutyCycle(float(args))
