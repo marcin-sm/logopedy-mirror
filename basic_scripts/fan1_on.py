4cf182f11c9c3a128124098a4a8da7f119110229
marcinsm <marcinsmietanka@gmail.com>
2018-12-17 21:02:26 +0100

    fan update GPIO.setwarnings(False)

--- basic_scripts/fan1_on.py
+++ basic_scripts/fan1_on.py
@@ -1,5 +1,6 @@
 import RPi.GPIO as GPIO
 fan1 = 27
 GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
 GPIO.setup(fan1,GPIO.OUT)
 GPIO.output(fan1, 1)
--- basic_scripts/fan2_on.py
+++ basic_scripts/fan2_on.py
@@ -1,5 +1,6 @@
 import RPi.GPIO as GPIO
 fan2 = 22
 GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
 GPIO.setup(fan2,GPIO.OUT)
 GPIO.output(fan2, 1)
