af124ddcdc1f728c268a7bb38c3117f0a0ad512e
marcinsm <marcinsmietanka@gmail.com>
2018-12-17 18:04:19 +0100

    updating script change

--- update.sh
+++ update.sh
@@ -2,5 +2,3 @@ cd /home/pi/black-mirror/
 
 git fetch
 git reset --hard origin/master
-
-/home/pi/arduino-1.8.5/arduino --board esp8266:esp8266:d1_mini:CpuFrequency=80,FlashSize=4M1M --port /dev/ttyUSB0 --upload /home/pi/black-mirror/esp_logic/esp_logic.ino
