7b665398bdef2dbb69d0a006bcfe4c27ef5b4d6e
marcinsm <marcinsmietanka@gmail.com>
2018-12-17 20:55:25 +0100

    added FAN functionality

---
+++ basic_scripts/fan1_off.py
@@ -0,0 +1,5 @@
+import RPi.GPIO as GPIO
+fan1 = 27
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(fan1,GPIO.OUT)
+GPIO.output(fan1, 0)
---
+++ basic_scripts/fan2_off.py
@@ -0,0 +1,5 @@
+import RPi.GPIO as GPIO
+fan2 = 22
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(fan2,GPIO.OUT)
+GPIO.output(fan2, 0)
