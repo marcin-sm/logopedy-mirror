1ec5d7d1b833c6db1a78f3e0faa804260779eb73
marcinsm <marcinsmietanka@gmail.com>
2018-12-29 14:52:40 +0100

    UPDATED TV script

--- basic_scripts/TV.py
+++ basic_scripts/TV.py
@@ -1,4 +1,5 @@
 import RPi.GPIO as GPIO
+import sys
 relayTV = 20
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(relayTV,GPIO.OUT)
@@ -7,7 +8,7 @@ def on ():
     GPIO.output(relayTV, 0)
 
 def off ():
-    GPIO.output(relayTV, 0)
+    GPIO.output(relayTV, 1)
 
 if __name__ == "__main__":
     args =  sys.argv[1:]
--- basic_scripts/TVon.py
+++
@@ -1,5 +0,0 @@
-import RPi.GPIO as GPIO
-relayTV = 20
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(relayTV,GPIO.OUT)
-GPIO.output(relayTV, 0)
