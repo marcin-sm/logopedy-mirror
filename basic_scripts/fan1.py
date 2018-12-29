28ad641ff59d33f47a0ba1c8537c078b396f95e8
marcinsm <marcinsmietanka@gmail.com>
2018-12-29 13:18:35 +0100

    UPDATED fans script

---
+++ basic_scripts/fan1.py
@@ -0,0 +1,14 @@
+import RPi.GPIO as GPIO
+fan1 = 27
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
+GPIO.setup(fan1,GPIO.OUT)
+
+if __name__ == "__main__":
+    args =  sys.argv[1:]
+    if (args[0])=='on':
+        GPIO.output(fan1, 1)
+    elif (args[0])=='off':
+        GPIO.output(fan1, 0)
+    else:
+        print "unknown parameter"
---
+++ basic_scripts/fan2.py
@@ -0,0 +1,14 @@
+import RPi.GPIO as GPIO
+fan2 = 22
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
+GPIO.setup(fan1,GPIO.OUT)
+
+if __name__ == "__main__":
+    args =  sys.argv[1:]
+    if (args[0])=='on':
+        GPIO.output(fan2, 1)
+    elif (args[0])=='off':
+        GPIO.output(fan2, 0)
+    else:
+        print "unknown parameter"
