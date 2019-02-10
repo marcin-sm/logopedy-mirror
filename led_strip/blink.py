006901840e3071cc25ab153f56eded708cb266f6
marcinsm <marcinsmietanka@gmail.com>
2019-02-10 16:28:50 +0100

    UPATED blink

--- led_strip/blink.py
+++ led_strip/blink.py
@@ -18,27 +18,27 @@ def brightness_decrease(pixels, R, G, B, wait=0.0001, step=1):
     wait = (255-max(R, G, B)/max(R, G, B))/10000000
     for j in range(int(256 // step)):
 
-            r, b, g = pixels.get_pixel_rgb(5)
+            r, b, g = pixels.get_pixel_rgb(4)
             r = int(max(0, r - step))
             g = int(max(0, g - step))
             b = int(max(0, b - step))
-            pixels.set_pixel(5, Adafruit_WS2801.RGB_to_color( r, b, g ))
-        pixels.show()
-        if wait > 0:
-            time.sleep(wait)
+            pixels.set_pixel(4, Adafruit_WS2801.RGB_to_color( r, b, g ))
+            pixels.show()
+            if wait > 0:
+                time.sleep(wait)
 
 def brightness_increase(pixels, R, G, B, wait=0.001, step=1):
     wait = (255-max(R, G, B)/max(R, G, B))/10000000
     for j in range(int(256 // step)):
 
-            r, b, g = pixels.get_pixel_rgb(5)
+            r, b, g = pixels.get_pixel_rgb(4)
             r = int(min(R, r + step))
             g = int(min(G, g + step))
             b = int(min(B, b + step))
-            pixels.set_pixel(5, Adafruit_WS2801.RGB_to_color( r, b, g ))
-        pixels.show()
-        if wait > 0:
-            time.sleep(wait)
+            pixels.set_pixel(4, Adafruit_WS2801.RGB_to_color( r, b, g ))
+            pixels.show()
+            if wait > 0:
+                time.sleep(wait)
 
 # Clear all the pixels to turn them off.
 
@@ -47,14 +47,19 @@ def pulse (color, repeat, step):
         R=85
         G=0 
         B=0
-    elif color == "orange": 
-        R=252
-        G=215 
-        B=0
+    elif color == "white": 
+        R=255
+        G=255 
+        B=255
     elif color == "green": 
         R=0
         G=255 
         B=0
+
+    elif color == "pink": 
+        R=255
+        G=20 
+        B=147
     else:
         R=255
         G=255 
@@ -66,5 +71,6 @@ def pulse (color, repeat, step):
 
 if __name__ == "__main__":
     args =  sys.argv[1:]
-    pulse(args[0],2,2)
+while True:
+    pulse(args[0],2,1)
 
